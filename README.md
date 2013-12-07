7efcf12dacf1d85350f88b6509ac6ba48d57c759
volodymyr_kulibaba <kvladimir2007@gmail.com>
2013-12-07 14:29:55 +0200

    test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-Perfume
-=======
+
+Installation verndors libraries
+http://package.json.nodejitsu.com/
+http://blog.nodejitsu.com/package-json-cheat-sheet
+
+About Express Framework
+http://expressjs.com/
+https://github.com/visionmedia/express/wiki
+
+Jade Tempalte Engine
+http://jade-lang.com/
+
+
+Tools:
+Html To Jade
+	http://html2jade.aaron-powell.com/
---
+++ r.bat
@@ -0,0 +1,10 @@
+:: http://package.json.nodejitsu.com/
+:: http://blog.nodejitsu.com/package-json-cheat-sheet
+:: This command locks down the versions of a package's dependencies so that you can control exactly 
+:: which versions of each dependency will be used when your package is installed. The "package.json" 
+:: file is still required if you want to use "npm install".
+:: install packages
+npm install
+
+:: https://npmjs.org/doc/cli/npm-shrinkwrap.html
+:: npm shrinkwrap
