21a2a680e4c7364a7778c51daca928290a55297f
volodymyr_kulibaba <kvladimir2007@gmail.com>
2013-12-07 19:51:23 +0200

    use http

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 #!/bin/env node
 //  OpenShift sample Node application
+var http = require('http');
 var express = require('express');
 var fs      = require('fs');
 
@@ -7,5 +8,7 @@ function on_start(req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end("Hello, world!")
 }
+
 var server = http.createServer(on_start);
+// var server = express.createServer(on_start);
 server.listen(process.env.PORT);
